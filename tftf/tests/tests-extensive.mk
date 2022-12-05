309d95eb37808ee136e42015f81690cd5c8972bf
Manish V Badarkhe <Manish.Badarkhe@arm.com>
2022-12-05 12:22:00 +0000

    refactor: add new test-suite 'tests-timer-stress'
    Moved 'Stress test timer framework' as part of 'tests-extensive' suite
    by creating a separate test suite for timers, i.e. tests-timer-stress.
    
    Signed-off-by: Manish V Badarkhe <Manish.Badarkhe@arm.com>
    Change-Id: Ibdad88e81fba55333c0ad17cc9c10d24d97e6c7e

--- tftf/tests/tests-extensive.mk
+++ tftf/tests/tests-extensive.mk
@@ -6,6 +6,8 @@
 
 # Run all standard tests, plus the extensive ones.
 include tftf/tests/tests-standard.mk
-TESTS_MAKEFILE += tftf/tests/tests-psci-extensive.mk
+
+TESTS_MAKEFILE += tftf/tests/tests-psci-extensive.mk	\
+		  tftf/tests/tests-timer-stress.mk
 
 include ${TESTS_MAKEFILE}
---
+++ tftf/tests/tests-timer-stress.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (c) 2022, Arm Limited. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+TESTS_SOURCES  +=                                              \
+       $(addprefix tftf/tests/framework_validation_tests/,     \
+               test_timer_framework.c                          \
+       )
